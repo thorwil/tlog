2d02348a3364af0288a3ca4eee50399f8322db46
Thorsten Wilms <t_w_@freenet.de>
2012-11-03 14:13:44 +0100

    Init.

---
+++ immutant.clj
@@ -0,0 +1,28 @@
+(ns tlog.init
+  ;(:use tlog.core)
+  (:require [immutant.messaging :as messaging]
+            [immutant.web :as web]
+            [immutant.utilities :as util]))
+
+;; This file will be loaded when the application is deployed to Immutant, and
+;; can be used to start services your app needs. Examples:
+
+
+;; Web endpoints need a context-path and ring handler function. The context
+;; path given here is a sub-path to the global context-path for the app
+;; if any.
+
+; (web/start "/" my-ring-handler)
+; (web/start "/foo" a-different-ring-handler)
+
+;; To start a Noir app:
+; (server/load-views (util/app-relative "src/tlog/views"))
+; (web/start "/" (server/gen-handler {:mode :dev :ns 'tlog}))
+
+
+;; Messaging allows for starting (and stopping) destinations (queues & topics)
+;; and listening for messages on a destination.
+
+; (messaging/start "/queue/a-queue")
+; (messaging/listen "/queue/a-queue" #(println "received: " %))
+
