23882326bd39d21d0777495eebc5173fd533c93b
Thorsten Wilms <t_w_@freenet.de>
2012-11-05 13:06:56 +0100

    Set :context-path to root.

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
 		 [net.cgrand/moustache "1.1.0"]
                  [org.clojure/tools.nrepl "0.2.0-beta10"]]
-  :immutant {:nrepl-port 4242})
+  :immutant {:nrepl-port 4242
+             :context-path "/"})
