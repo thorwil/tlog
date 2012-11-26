6beb14f278f6c0779726b1585fb340f43c3da4f1
Thorsten Wilms <t_w_@freenet.de>
2012-11-26 13:31:14 +0100

    Add tests for tlog.data.account/accounts-raw.

--- test/tlog/data/t_account.clj
+++ test/tlog/data/t_account.clj
@@ -2,6 +2,25 @@
   (:require [midje.sweet :refer [fact tabular]]
             [tlog.data.account]))
 
+;; Use @#'tlog.data.account/accounts-raw to get at the value of the private var.
+(fact "accounts-raw database query result includes 'admin' key"
+  (some #(= "admin" %) (map :username @#'tlog.data.account/accounts-raw)) => true)
+
+
+(defn some-equal
+  "True if 'i' is in sequence 'items', else false."
+  [i items]
+  (some #(= % i) items))
+
+(defn remove-elements
+  "Remove items in the seq 'remove-these' from items in seq 'items'."
+  [remove-these items]
+  (reduce #(if (some-equal %2 remove-these) %1 (cons %2 %1)) '() items))
+
+(fact "accounts-raw database query result contains only :username and :password keys"
+  (remove-elements [:username :password] (mapcat keys @#'tlog.data.account/accounts-raw)) => empty?)
+
+
 (def accounts-1-raw
   [{:password "$2a$10$Cp0GK58KbTE3SYx8wmgqLO2oF2JHLp/a72D6UGevvKCwfzdPHqBHu"
     :username "admin"}])
