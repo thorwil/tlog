fe5525f6516597ff59b2cd626dab5a6de6f7f4f4
Thorsten Wilms <t_w_@freenet.de>
2012-12-10 21:12:45 +0100

    Replace var-as-map acrobatics with macros (defopt and defopt-fn).

--- src/tlog/render/html.clj
+++ src/tlog/render/html.clj
@@ -6,6 +6,58 @@
             [hiccup.page :refer [html5 include-css]]
             [tlog.render.configuration :as conf]))
 
+;; Taken from Clojure 1.2 clojure.contrib.def:
+(defn- name-with-attributes
+  "To be used in macro definitions. Handles optional docstrings and attribute maps for a name to be
+   defined in a list of macro arguments. If the first macro argument is a string, it is added as a
+   docstring to name and removed from the macro argument list. If afterwards the first macro
+   argument is a map, its entries are added to the name's metadata map and the map is removed from
+   the macro argument list. The return value is a vector containing the name with its extended
+   metadata map and the list of unprocessed macro arguments."
+  [name macro-args]
+  (let [[docstring macro-args] (if (string? (first macro-args))
+                                 [(first macro-args) (next macro-args)]
+                                 [nil macro-args])
+        [attr macro-args] (if (map? (first macro-args))
+                            [(first macro-args) (next macro-args)]
+                            [{} macro-args])
+        attr (if docstring
+               (assoc attr :doc docstring)
+               attr)
+        attr (if (meta name)
+               (conj (meta name) attr)
+               attr)]
+    [(with-meta name attr) macro-args]))
+
+(defmacro ^:private defopt
+  "Macro for defining static, optional HTML fragments as single-keyword hash-maps, to be passed to
+   skeleton.
+
+   Take a name, optional doc-string, body and optional keyname. Use name to create a keyname, if
+   none is given. Return the body as value to the keyname in a hash-map."
+  [name* & more]
+  (let [[name body-and-maybe-keyword] (name-with-attributes name* more)
+        keyword* (or (second body-and-maybe-keyword)
+                     (keyword name))]
+    `(def ~name
+       {~keyword* ~(first body-and-maybe-keyword)})))
+
+(defmacro ^:private defopt-fn
+  "Macro for defining functions that return optional HTML fragments as single-keyword hash-maps, to
+   be passed to skeleton.
+
+   Take a name, optional doc-string, argument-list, body and optional keyname. Use name to create a
+   keyname, if none is given. Return a form that returns the body as value to the keyname in a
+   hash-map."
+  [name* & more]
+  (let [[name args+body+maybe-keyword] (name-with-attributes name* more)
+        [args body] args+body+maybe-keyword
+        keyword* (or (nth args+body+maybe-keyword 2 nil)
+                     (keyword name))]
+    `(defn ~name
+       ~args
+       {~keyword* ~body})))
+
 (def title-with
   "Build string for the <head>, <title> tag."
   #(str %
@@ -53,7 +105,7 @@
        [:input {:type "password" :name "password" :value ""}]]]]
     [:input {:type "submit" :value "submit"}]]))
 
-(def option-noscript-warning
+(defopt option-noscript-warning
   (html [:noscript [:div#noscript-warning "This won't work with JavaScript disabled ;)"]]))
 
 (def feed-selector
--- src/tlog/render/page.clj
+++ src/tlog/render/page.clj
@@ -2,24 +2,17 @@
   "Take data from tlog.dispatch.handle and build HTML responses, using tlog.render.html."
   (:require [tlog.render.html :as h]))
 
-(defn- var-as-map
-  "Take a #'var or a quoted sexp starting with a #'var. Return a map with the var's :name as keyword
-   and the var's value as body."
-  [v]
-  (if (list? v)
-    ;; Argument is a list (and should be a quoted sexp starting with a #'var)
-    {(-> v first eval meta :name keyword) (eval v)}
-    ;; Argument is a not a list (and should be a #'var)
-    {(-> v meta :name keyword) (var-get v)}))
-
 (def journal
-  (h/skeleton {:title "Journal" :buildup "Journal"}))
+  (h/skeleton {:title "Journal"
+               :buildup "Journal"}))
 
 (def login
-  (h/skeleton {:title "Login" :buildup h/login-form}))
+  (h/skeleton {:title "Login"
+               :buildup h/login-form}))
 
 (def admin
-  (h/skeleton {:title "Admin" :buildup "Admin"}))
+  (h/skeleton {:title "Admin"
+               :buildup "Admin"}))
 
 (def write*
   (h/skeleton {:title "Writer"
@@ -30,7 +23,8 @@
   (h/skeleton (into  {:title "Writer"
                       :scripts h/aloha-admin
                       :buildup h/article-form}
-                     (var-as-map #'h/option-noscript-warning))))
+                     h/option-noscript-warning)))
 
 (def not-found
-  (h/skeleton {:title "Write" :buildup "404: There's nothing associated with this URL."}))
+  (h/skeleton {:title "Write"
+               :buildup "404: There's nothing associated with this URL."}))
