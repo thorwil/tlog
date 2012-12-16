a251a42e45512e66d88d1e6c679035055307c4b6
Thorsten Wilms <t_w_@freenet.de>
2012-12-16 13:31:17 +0100

    Queries for article-feed relations.

--- src/tlog/data/feed.clj
+++ src/tlog/data/feed.clj
@@ -31,3 +31,19 @@
   [article-slug feed-slugs]
   (d/delete-rows "article_feed_rel" (format "article_slug = %s" article-slug))
   (create-article-feed-rels! article-slug feed-slugs))
+
+(defn article-slugs-in-feed
+  "Take a feed slug. Return a list of slugs for all articles the feed is associated with."
+  [feed-slug]
+  (d/with-connection db
+    (d/with-query-results rs [(format "SELECT article_slug FROM article_feed_rel
+                                                           WHERE feed_slug = '%s';" feed-slug)]
+      (map :article_slug (into [] rs)))))
+
+(defn feed-slugs-for-article
+  "Take an article slug. Return a list of the slugs for the feeds the article is associated with."
+  [article-slug]
+  (d/with-connection db
+    (d/with-query-results rs [(format "SELECT feed_slug FROM article_feed_rel
+                                                        WHERE article_slug = '%s';" article-slug)]
+      (map :feed_slug (into [] rs)))))
